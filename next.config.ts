f1cbd46a2b447a6e4b8f0f6359cd96d5ba9fc724
JahziRam <jahaziela@ramanitra.mg>
2026-07-06 03:58:13 +0300

    fix: add pg-cloudflare dependency for Cloudflare Workers build

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: ["pg", "pg-cloudflare", "@prisma/client", "@prisma/adapter-pg"],
   images: {
     remotePatterns: [{ protocol: "https", hostname: "**" }],
   },
